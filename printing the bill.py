6452f4e4bb062c96ba0ef5be4ac6df8bed123a59
suneetha5522 <47383007+suneetha5522@users.noreply.github.com>
2019-02-08 09:55:56 -0800

    Add files via upload

---
+++ printing the bill.py
@@ -0,0 +1,32 @@
+
+print("VIJETHA".center(30))
+print("SUPER MARKET".center(30,'*'))
+print("Date 08 Feb 2019".center(40))
+print("-"*30)
+a,b,c,d,e=2,3,5,6,4
+x,y,z,p,q=100,200,40,50,30
+
+print("  1   " + " soups     " + " {}      {}".format(str(a),a*x))
+print("  2   " + " brushs    " + " {}      {}".format(str(b),b*y))
+print("  3   " + " power     " + " {}      {}".format(str(c),c*z))
+print("  4   " + " magi      " + " {}      {}".format(str(d),d*p))
+print("  5   " + " batteries " + " {}      {}".format(str(e),e*q))
+print("-"*30)
+n=(a*x)+(b*y)+(c*z)+(d*p)+(e*q)
+GST=n*0.03
+total=n+GST
+print("sub total".center(23),n)
+print("GST@ 3%".center(23),GST)
+print("-"*30)
+print("TOTAL".center(23),n)
+
+
+
+
+
+
+
+
+
+
+
---
+++ string methods.py
@@ -0,0 +1,82 @@
+string1="hello how are you"
+string2="PYTHON is a interpreter language"
+#print(dir(string))
+
+#capilaze() Method:-
+print("python Is Dynamic Language".capitalize())
+
+#Case Fold() Method:-
+print("QizzBHHHH".casefold())
+
+#Center method:-
+print(string1.center(50,"#"))
+print("VIJETHA SUPER MARKET".center(30))
+
+#Count Method:-
+print("let it be let it be".count("e",1,40))
+print("aeiouaeiou".count("u"))
+print(string2.count("r",10,40))
+
+#Encode method:-
+print("'pythön!".encode('UTF-8','strict'))
+print("'pythön!".encode('ascii','replace'))
+print("'pythön!".encode('ascii','xmlcharrefreplace'))
+print("'pythön!".encode('ascii','backslashreplace'))
+print("'pythön!".encode('ascii','backslashreplace'))
+print("pythön!".encode())
+
+#endswith():-
+print(string2.endswith("language"))
+print((string2.endswith("is",0,20)))
+
+#startswith():-
+print("hi good morning".startswith("hi"))
+print("hi good morning".startswith("morning",8,20))
+
+#Expand tab:-
+print("pyth\t0n".expandtabs(8))
+
+#Find ():-
+print("let it be let it be".find("be"))
+print("let it be".find("small"))
+
+#index():-
+print("hi good morning".index("good"))
+print("hi good morning".index("good",3,-1))
+
+#isalnum:-
+print("hi23".isalnum())
+print("hi 123".isalnum())
+
+#isalpha:-
+print("hi".isalpha())
+print("hi 123".isalpha())
+
+#isdecimal:-
+print("1213".isdecimal())
+print("abc123".isdecimal())
+
+#isdigit:-
+print("123".isdigit())
+
+#lower
+print("SUNEETHA".lower())
+
+
+
+
+
+
+
+
+
+
+
+ #'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable',
+  #'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind',
+  #'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase',
+  #'title', 'translate', 'upper', 'zfill']
+""" ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', 
+'__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', 
+'__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
+ '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook """
