fe256076c6514c87678da3697a26e784a3ee7c82
suneetha5522 <47383007+suneetha5522@users.noreply.github.com>
2019-02-14 01:09:53 -0800

    Add files via upload

--- List_Assignment.py
+++ List_Assignment.py
@@ -1,15 +1,31 @@
-'''Remove all Nones in to string without using for loop'''
-#Input values given to list
-list1=[1,2,3,None,"ram","Ramesh",24,56,None,10]
-res=list1.index(None)    #finding the None in List1
-list1.remove(None)             #Deleting the None in list1
-list1.insert(res,"string")     #updating  First None with String
-print(list1)
+#Replace None with String in list
+list1=[1,2,3,4,5,6,7,None,8,9,10]
+list1[7]="String"       #updating value with String
+print("The Updating list is:",list1)
 
-res1=list1.index(None)    #finding the None in List1
-list1.remove(None)             #Deleting the None in list1
-list1.insert(res1,"string")    #updating Second None with String
-print(list1)
+
+#In list all 1's are chnages to None
+list2=[1,2,3,1,2,3,1,2,3]
+res1=list2.index(1)             #finding the index is 1st one
+list2.remove(1)                 #Removing the 1st one
+list2.insert(res1,None)         #inserting 1 with None
+#print(list2)
+
+res2=list2.index(1)             #finding the index is 1st one
+list2.remove(1)                 #Removing the 1st one
+list2.insert(res2,None)         #inserting 1 with None
+#print(list2)
+
+res3=list2.index(1)              #finding the index is 1st one
+list2.remove(1)                  #Removing the 1st one
+list2.insert(res3,None)          #inserting 1 with None
+print("The updating List is :",list2)
+
+
+
+
+
+
 
 
 
