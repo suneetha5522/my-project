f6053fee5c2f7a6df4b7424b5d5ce60b648bde5d
suneetha5522 <47383007+suneetha5522@users.noreply.github.com>
2019-02-13 21:34:21 -0800

    Add files via upload

---
+++ List_Assignment.py
@@ -0,0 +1,9 @@
+
+list1=[1,2,3,"ram","Ramesh","None",45,90]  #Assigning values to the list
+list1.pop(5)                               #Deleting the None from List
+list1.insert(5,"True")                      #Inserting the Ture in place of None
+print(list1)
+
+list2=[1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1]
+print(list2.count(1))
+
