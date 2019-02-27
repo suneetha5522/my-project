8ef3c6ef45228693a00b8324beb64a77cbab538e
suneetha5522 <47383007+suneetha5522@users.noreply.github.com>
2019-02-27 12:28:51 +0530

    Add files via upload

--- class_family.py
+++ class_family.py
@@ -1,6 +1,6 @@
 """
 create a class for family members
-1.add a method for family members are the in family class
+1.add a method for family members and find no of families
 2. Remove one family member with ID
 """
 
@@ -16,10 +16,10 @@ class Family:
     def add_member_family(self, name, age, mid):
         """
         adding the member to family list
-        :param name: variable
-        :param age: variable
-        :param mid: variable
-        :return: appending the variables to family list
+        :param name: string,name of the person
+        :param age: integer,age of the person
+        :param mid: integer,id if the person
+        :return: dictionary
         """
         d = {"name": name, 'age': age, "id": mid}
         self.family.append(d)
@@ -27,33 +27,35 @@ class Family:
 
     def no_of_families(self):
         """
-        finding the how many families are present in the list of families
-        :return: length of the family
+        finding the total no of families are present in the list
+        :return: integer,total no of families
         """
         return len(self.family)
 
     def remove_one_family_by_id(self, mid):
         """
-        deleting the one family by using the ID
-        :param mid: id in family class
-        :return: deleted family by id
+        delete the one family by using the ID
+        :param mid: integer, id of the family class
+        :return: boolean, True or False
         """
 
         for i, d in enumerate(self.family):
             if d.get("id", mid) == mid:
-                del i
-                return d
+                del self.family[i]
+                return True
+
 
 x = Family()
-
 x.add_member_family("charan", 23, 12345)
 x.add_member_family("ravi", 25, 123456)
 x.add_member_family("kumar", 27, 123457)
 x.add_member_family("nagesh", 30, 1234556)
 x.add_member_family("suneetha", 23, 12345568)
-print("The familes are:", x.family)
+print("The families are:", x.family)
 
 y = x.no_of_families()
 print("The total no.of families are:", y)
+#print(x.family)
 z = x.remove_one_family_by_id(12345)
+#print(x.family)
 print("The deleted family by using id:", z)
